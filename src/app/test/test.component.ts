7f195f050fa10a784ece990276d1386029bab9cd
Rapha-Y <raph.yuki@gmail.com>
2020-05-17 20:36:56 -0300

    addded test component

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-test',
+    template: `
+        <h2>
+            Welcome Alice
+        </h2>  
+    `,
+    styles: []
+})
+
+export class TestComponent implements OnInit {
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
