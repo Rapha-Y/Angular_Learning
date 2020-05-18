b9d74f5540b616883330057872468b9057cfeadc
Rapha-Y <raph.yuki@gmail.com>
2020-05-18 00:12:34 -0300

    added input with two-way binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,20 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-test',
     template: `
-        <h2>
-            Welcome {{ name }}
-        </h2>
-        <input #myInput type='text'>
-        <button (click)='logMessage(myInput)'>
-            Log
-        </button>
+        <input [(ngModel)]="name" type="text">
+        {{ name }}
     `,
     styles: []
 })
 
 export class TestComponent implements OnInit {
 
-    public name = 'Alice';
+    public name = "";
 
     constructor() {
 
