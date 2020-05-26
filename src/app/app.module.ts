d8c8346f38cfc85eed2921caf262c4b255a9afff
Rapha-Y <raph.yuki@gmail.com>
2020-05-25 21:02:20 -0300

    added css from bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/courses-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/courses/courses-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './courses-list.component.html',
+})
+export class CourseListComponent {
+
+}
