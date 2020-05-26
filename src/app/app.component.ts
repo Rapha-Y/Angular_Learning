2f570c2e076bc5edf5555bac52ba5b8b4e6cde9b
Rapha-Y <raph.yuki@gmail.com>
2020-05-25 21:30:46 -0300

    added table of courses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-manager';
-  name = "Johnny";
+  title: string = 'course-manager';
+  name: string = "Johnny";
 }
--- src/app/courses/courses-list.component.ts
+++ src/app/courses/courses-list.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './courses-list.component.html',
 })
 export class CourseListComponent {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: "Angular: Forms",
+                imageUrl: "",
+                price: 99.99,
+                code: "XPS-8796",
+                duration: 120,
+                rating: 4.5,
+                releaseDate: "December, 2, 2019"
+            },
+            {
+                id: 2,
+                name: "Angular: Http",
+                imageUrl: "",
+                price: 45.99,
+                code: "LKL-1094",
+                duration: 80,
+                rating: 4,
+                releaseDate: "November, 4, 2019"
+            }
+        ]
+    }
 
 }
