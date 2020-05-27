8f0f71987841db23f4928682a8dadc708c80a5c2
Rapha-Y <raph.yuki@gmail.com>
2020-05-26 23:48:48 -0300

    added external data manipulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { CourseInfoComponent } from './courses/course-info.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: 'courses', component: CourseListComponent },
       { path: 'courses/info/:id', component: CourseInfoComponent },
